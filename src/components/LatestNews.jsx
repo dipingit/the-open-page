6758bb6cb28f835a9d1ca05a0a7394d9191c8cd8
dipingit <diproyf87@gmail.com>
2025-06-04 16:26:14 -0300

    designed main layout, added navbar

---
+++ src/components/LatestNews.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import { Link } from 'react-router-dom';
+
+const LatestNews = () => {
+    return (
+        <div className='flex gap-2 items-center bg-base-200 p-2'>
+            <p className='bg-[#D72050] text-base-100 px-3 py-1'>Latest</p>
+           
+            <Marquee pauseOnHover={true} className='space-x-10'>
+                <Link to='/news'>
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, quae.
+                </Link>
+                <Link to='/news'>
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, quae.
+                </Link>
+            </Marquee>
+        </div>
+    );
+};
+
+export default LatestNews;
